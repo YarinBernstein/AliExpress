f15d26526adf9ed277610077531f3e8df067dac5
Dor Ravid <dorravid878@gmail.com>
2023-08-23 08:34:10 +0300

    catch async functions errors

--- Backend/src/utils/catchAsync.ts
+++ Backend/src/utils/catchAsync.ts
@@ -1,9 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 
-const catchAsync = (asyncFunc: (req: Request, res: Response, next: NextFunction) => Promise<void>) => {
-    return (req: Request, res: Response, next: NextFunction) => {
-        asyncFunc(req, res, next).catch(next);
-    };
+const catchAsync = (
+	asyncFunc: (req: Request, res: Response, next: NextFunction) => Promise<void>
+) => {
+	return (req: Request, res: Response, next: NextFunction) => {
+		asyncFunc(req, res, next).catch(next);
+	};
 };
 
 export default catchAsync;
